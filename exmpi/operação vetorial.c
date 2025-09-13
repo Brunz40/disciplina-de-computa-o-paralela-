f03811a51903844d7a976832a26e9cb8566cb108
Brunz40 <brunodalcozac@usp.br>
2025-09-12 23:44:15 -0300

    Criado ambiente mpi

---
+++ exmpi/operação vetorial.c
@@ -0,0 +1,11 @@
+//
+// Created by bruno on 12/09/25.
+//
+
+#include "operação vetorial.h"
+float* somar(float* v1, float* v2, unsigned int length){
+
+}
+float* produto(float* v1, float* v2, unsigned int length) {
+
+}
---
+++ exmpi/operação vetorial.h
@@ -0,0 +1,9 @@
+//
+// Created by bruno on 12/09/25.
+//
+
+#ifndef OPERAÇÃO_VEORIAL_H
+#define OPERAÇÃO_VEORIAL_H
+float* somar(float* v1, float* v2, unsigned int length);
+float* produto(float* v1, float* v2, unsigned int length);
+#endif //OPERAÇÃO_VEORIAL_H
