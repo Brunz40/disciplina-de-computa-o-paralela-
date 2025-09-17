98f4e28b09cb4d5d5d2b1f00c3d189d21154a5b3
Brunz40 <brunodalcozac@usp.br>
2025-09-17 20:05:50 -0300

    produto interno consertado

--- exmpi/Main2.c
+++ exmpi/Main2.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &N_tarefas);
 
-    int carga=1000*N_tarefas;
+    int carga=4032;
     int N_envios=N_recebidos=carga/N_tarefas;
 
     float* buffer_origem_A;
@@ -38,11 +38,11 @@ int main(int argc, char **argv) {
     MPI_Scatter(buffer_origem_B,N_envios,MPI_FLOAT,buffer_destino_B,N_recebidos,MPI_FLOAT,cabeça,MPI_COMM_WORLD);
 
     float* resultado_soma=malloc((N_recebidos)*sizeof(float));
-    float* resultado_produto=malloc((N_recebidos)*sizeof(float));
+    float resultado_produto=0.0;
 
     for(int i=0; i<N_recebidos; i++) {
         resultado_soma[i]=buffer_destino_A[i]+buffer_destino_B[i];
-        resultado_produto[i]=buffer_destino_A[i]*buffer_destino_B[i];
+        resultado_produto+=buffer_destino_A[i]*buffer_destino_B[i];
     }
 
     MPI_Gather(buffer_destino_A,N_recebidos,MPI_FLOAT,buffer_retorno_A,N_envios,MPI_FLOAT,cabeça,MPI_COMM_WORLD);
@@ -51,7 +51,7 @@ int main(int argc, char **argv) {
     fim=MPI_Wtime();
 
     if (rank==cabeça) {
-        printf("tempo decorrido:%lf\ncarga total:%d\nNúmero de processadores utilizados:%d",fim-inicio,carga,N_tarefas);
+        printf("tempo decorrido:%lf\ncarga total:%d\nNúmero de processadores utilizados:%d\n",fim-inicio,carga,N_tarefas);
         free(buffer_origem_A);
         free(buffer_origem_B);
         free(buffer_retorno_A);
@@ -61,7 +61,6 @@ int main(int argc, char **argv) {
     free(buffer_destino_A);
     free(buffer_destino_B);
     free(resultado_soma);
-    free(resultado_produto);
 
     MPI_Finalize();
 
